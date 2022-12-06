eb00a00bd23ab1410a52952eb4cc011d4d2397a3
Tomczan <t.czuba95@gmail.com>
2022-12-06 21:01:55 +0100

    add User serializer
    sorted imports

--- shopping_tracker/orders/serializers.py
+++ shopping_tracker/orders/serializers.py
@@ -1,7 +1,23 @@
-from .models import *
-
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
 from rest_framework.validators import UniqueTogetherValidator
+
+from .models import *
+
+UserModel = get_user_model()
+
+
+class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = UserModel
+        field = ('id', 'username', 'password')
+
+    def create(self, validated_data):
+        user = UserModel.objects.create_user(**validated_data)
+
+        return user
 
 
 class BrandSerializer(serializers.ModelSerializer):
