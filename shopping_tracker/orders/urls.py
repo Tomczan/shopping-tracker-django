925e90498e3ccbb1128de5db17c6cbfd286a2869
Tomczan <t.czuba95@gmail.com>
2022-12-08 18:47:56 +0100

    add: api endpoint to obtain auth token by user

--- shopping_tracker/orders/urls.py
+++ shopping_tracker/orders/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
+from rest_framework.authtoken.views import obtain_auth_token
 
 from .views import *
 
 urlpatterns = [
     path('user/register/', UserCreateAPIView.as_view(), name="user-register"),
+    path('auth-token/', obtain_auth_token,
+         name="obtain-auth-token"),
     path('brand/', BrandListCreateAPIView.as_view(), name="brand-list"),
     path('product/', ProductListCreateAPIView.as_view(), name="product-list"),
     path('shop/', ShopListCreateAPIView.as_view(), name="shop-list"),
