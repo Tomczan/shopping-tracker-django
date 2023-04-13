f7dfa6dd64c06d2fe0aa2f915e7e63d8586da5c5
Tomczan <t.czuba95@gmail.com>
2023-04-13 22:53:08 +0200

    add JWT token obtain and refresh urls

--- shopping_tracker/orders/urls.py
+++ shopping_tracker/orders/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
-from rest_framework.authtoken.views import obtain_auth_token
+from rest_framework_simplejwt.views import (TokenObtainPairView,
+                                            TokenRefreshView)
 
 from .views import *
 
 urlpatterns = [
     path('user/register/', UserCreateAPIView.as_view(), name="user-register"),
-    path('auth-token/', obtain_auth_token,
-         name="obtain-auth-token"),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('brand/', BrandListCreateAPIView.as_view(), name="brand-list"),
     path('brand/<int:pk>/update', BrandUpdateAPIView.as_view(), name="brand-update"),
     path('brand/<int:pk>/delete', BrandDestroyAPIView.as_view(), name="brand-delete"),
