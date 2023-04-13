87983edeec9ff2d7d6ba0d99e9c1e9cf7a38fc2e
Tomczan <t.czuba95@gmail.com>
2023-04-13 22:37:22 +0200

    changed rest framework default auth to JWT token

--- shopping_tracker/shopping_tracker/settings.py
+++ shopping_tracker/shopping_tracker/settings.py
@@ -138,6 +138,6 @@ REST_FRAMEWORK = {
     #     'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
     # ]
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
 }
