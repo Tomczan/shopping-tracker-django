b39ec88c7f45338afac38add58e4a9cd569c40ca
Tomczan <t.czuba95@gmail.com>
2023-04-18 21:32:31 +0200

    Fix typo

--- shopping_tracker/shopping_tracker/settings.py
+++ shopping_tracker/shopping_tracker/settings.py
@@ -50,18 +50,18 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'shopping_tracker.urls'
 
 # https://pypi.org/project/django-cors-headers/
-CORS_ALLOW_ALL_ORIGINS: True
+CORS_ALLOW_ALL_ORIGINS = True
 
 TEMPLATES = [
     {
